a2ba240b12d7c912f3efa50d52a2dbebcc9b6701
gibranoderenz <gibranoderenz123@gmail.com>
2022-10-15 15:40:02 +0700

    Create get crowdfunds view

--- crowdfunding/views.py
+++ crowdfunding/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from .models import Crowdfund
 
 # Create your views here.
 def show_crowdfundings(request):
-    pass
+    crowdfunds = Crowdfund.objects.all()
+    context = {"crowdfunds": crowdfunds}
+    return render(request, "crowdfunding.html", context)
