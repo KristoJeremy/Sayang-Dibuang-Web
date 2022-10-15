2f1d79f33da6514bdb2441729ebcdf1f4f5586b1
gibranoderenz <gibranoderenz123@gmail.com>
2022-10-15 21:27:14 +0700

    Modify show crowdfunding view

--- crowdfunding/views.py
+++ crowdfunding/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from .models import Crowdfund
 
-# Create your views here.
+
 def show_crowdfundings(request):
     crowdfunds = Crowdfund.objects.all()
     context = {"crowdfunds": crowdfunds}
