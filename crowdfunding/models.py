eb8b6e8c678a7cf3ed415c887d672f6408ce94f6
gibranoderenz <gibranoderenz123@gmail.com>
2022-10-15 15:32:17 +0700

    Create crowdfund model

--- crowdfunding/models.py
+++ crowdfunding/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Crowdfund(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    received = models.IntegerField()  # number of items received by the user
+    target = models.IntegerField()  # number of items needed by the user
+    is_accomplished = models.BooleanField(
+        default=False
+    )  # is the target of the crowdfund is achieved?
