40531e3b7d5fbaf7154502f0131e059e1cd9c86d
Lyzander Marciano Andrylie <lyzanderandrylie@gmail.com>
2022-10-20 22:01:12 +0700

    Add validation to telephone field

--- fitur_autentikasi/forms.py
+++ fitur_autentikasi/forms.py
@@ -1,8 +1,36 @@
 from django import forms
-from django import forms
 from fitur_autentikasi.models import Profile
 
+import re
+
 class ProfileForm(forms.ModelForm):
+
+    def clean_telephone(self):
+        data = self.cleaned_data["telephone"]
+
+        regex_for_phone_number = r"(0|\+{0,2}[0-9]{2})[0-9]{9,11}"
+
+        if (re.match(regex_for_phone_number, data)):
+            return data 
+
+        raise forms.ValidationError("Phone number is not valid!")
+
+    def clean_whatsapp(self):
+        data = self.cleaned_data["whatsapp"]
+
+        regex_for_phone_number = r"(0|\+{0,2}[0-9]{2})[0-9]{9,11}"
+
+        if (re.match(regex_for_phone_number, data)):
+            return data 
+
+        raise forms.ValidationError("Whatsapp is not valid!")
+
     class Meta:
         model = Profile
         fields = ('telephone', 'whatsapp', 'line')
+    
+    telephone = forms.CharField(label="telephone", widget=forms.TextInput(attrs={"placeholder": "Start with 0/+XX/XX"}))
+    whatsapp = forms.CharField(label="whatsapp", widget=forms.TextInput(attrs={"placeholder": "Start with 0/+XX/XX"}))
+    line = forms.CharField(label="line")
+
+    
--- fitur_autentikasi/models.py
+++ fitur_autentikasi/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 # Create your models here.
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    telephone = models.CharField(max_length=12)
-    whatsapp = models.CharField(max_length=12)
+    telephone = models.CharField(max_length=15)
+    whatsapp = models.CharField(max_length=15)
     line = models.CharField(max_length=100)
     poin = models.IntegerField(default=0)
