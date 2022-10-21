d57a1d4afc908972add2c6d682d24a1bba2229c6
Lyzander Marciano Andrylie <lyzanderandrylie@gmail.com>
2022-10-21 15:57:28 +0700

    modify test_app

--- test_app/views.py
+++ test_app/views.py
@@ -1,12 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from fitur_autentikasi.models import Profile
 
 # Create your views here.
 def hello_world(request):
-
-    user = Profile.objects.get(user=request.user)
-    print(request.user.first_name)
-    print(user.get_fullname())
-
-    return HttpResponse(user.get_fullname())
+    return HttpResponse("Hello, world!")
