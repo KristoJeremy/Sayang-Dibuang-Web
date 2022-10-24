65375ce0712f80328e14a58b56c4acfab92ebd4f
Catherine Angel <catheriner40540@gmail.com>
2022-10-24 21:02:01 +0700

    add delete barang, upload barang button

--- barang_bekas/urls.py
+++ barang_bekas/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from barang_bekas.views import create_barang, get_all_barang_json, get_one_barang_json, show_barang, show_barang_detail, edit_barang, create_kategori, create_lokasi
+from barang_bekas.views import create_barang, get_all_barang_json, get_one_barang_json, show_barang, show_barang_detail, edit_barang, create_kategori, create_lokasi, delete_barang
 
 app_name = 'barang_bekas'
 
@@ -9,6 +9,7 @@ urlpatterns = [
     path('all/', get_all_barang_json, name="get_all_barang_json"),
     path('<int:id>/', show_barang_detail, name="show_barang_detail"),
     path('<int:id>/edit/', edit_barang, name="edit_barang"),
+    path('<int:id>/delete/', delete_barang, name="delete_barang"),
     path('upload/', create_barang, name="create_barang"),
     path('add/kategori/', create_kategori, name="create_kategori"),
     path('add/lokasi/', create_lokasi, name="create_lokasi")
