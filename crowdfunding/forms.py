a0a68d4b4cf273f846486dac92be7837700be5c0
gibranoderenz <gibranoderenz123@gmail.com>
2022-10-28 23:43:48 +0700

    Add styles to form page

--- crowdfunding/forms.py
+++ crowdfunding/forms.py
@@ -17,7 +17,9 @@ class CrowdfundForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         for visible in self.visible_fields():
-            visible.field.widget.attrs["class"] = "form-control mb-4"
+            visible.field.widget.attrs[
+                "class"
+            ] = "form-control mb-4 border-0 py-2 text-secondary"
 
     def clean_target(self):
         received = self.cleaned_data["received"]
