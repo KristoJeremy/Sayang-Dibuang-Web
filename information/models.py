5bc20082ce96c54fca7b3b09524827a0ef0c7b6a
khansajovita <khansajovita@gmail.com>
2022-10-29 01:45:15 +0700

    add review dkk

--- information/models.py
+++ information/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from fitur_autentikasi.models import Profile
+from django.contrib.auth.models import User
+from tinymce.models import HTMLField
 
-# Create your models here.
+
+class Review (models.Model):
+    # user = models.ForeignKey(
+    #     Profile, on_delete=models.CASCADE, blank=True, null=True, to_field="user"
+    # )
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    judul = models.CharField(max_length = 255)
+    deskripsi = HTMLField()
+    created = models.DateTimeField(auto_now_add = True)
+
