698798ef6e0fd8509c1a946f78b83e7f3fba0487
gibranoderenz <gibranoderenz123@gmail.com>
2022-10-28 16:06:19 +0700

    Fix crowdfund card order

--- crowdfunding/views.py
+++ crowdfunding/views.py
@@ -5,6 +5,7 @@ from django.contrib.auth.decorators import login_required
 from .models import Crowdfund
 from .forms import CrowdfundForm
 from fitur_autentikasi.models import Profile
+from django.contrib.auth.models import User
 
 
 @login_required(login_url="/login/")
@@ -12,12 +13,12 @@ def show_crowdfundings(request):
     return render(request, "home.html")
 
 
-@login_required(login_url="/login/")
+@login_required(login_url="/todolist/login/")
 def show_crowdfunding_by_id(request, id):
     return render(request, "crowdfunding_by_id.html")
 
 
-@login_required(login_url="/login/")
+@login_required(login_url="/todolist/login/")
 def create_crowdfund(request):
     form = CrowdfundForm()
 
@@ -27,17 +28,15 @@ def create_crowdfund(request):
             new_crowdfund = form.save(commit=False)
             new_crowdfund.user = Profile.objects.get(user=request.user)
             new_crowdfund.save()
-            return redirect("crowdfunding:show_crowdfundings")
+            return redirect("/crowdfundings")
 
     context = {"form": form}
     return render(request, "crowdfund_form.html", context)
 
 
-@login_required(login_url="/login/")
+@login_required(login_url="/todolist/login/")
 def edit_crowdfund(request, id):
     crowdfund = Crowdfund.objects.get(pk=id)
-    if crowdfund.user.pk != request.user.pk:
-        return redirect("crowdfunding:show_crowdfundings")
 
     form = CrowdfundForm(instance=crowdfund)
 
@@ -45,30 +44,38 @@ def edit_crowdfund(request, id):
         form = CrowdfundForm(request.POST, instance=crowdfund)
         if form.is_valid():
             form.save()
-            return redirect("crowdfunding:show_crowdfundings")
+            return redirect("/crowdfundings")
 
     context = {"form": form}
     return render(request, "crowdfund_form.html", context)
 
 
-@login_required(login_url="/login/")
+@login_required(login_url="/todolist/login/")
 def delete_crowdfund(request, id):
     crowdfund = Crowdfund.objects.get(pk=id)
-    if crowdfund.user.pk != request.user.pk:
-        return redirect("crowdfunding:show_crowdfundings")
     crowdfund.delete()
-    return redirect("crowdfunding:show_crowdfundings")
+    return redirect("/crowdfundings")
 
 
-@login_required(login_url="/login/")
+@login_required(login_url="/todolist/login/")
 def show_crowdfundings_json(request):
-    crowdfunds = Crowdfund.objects.all().order_by("-created")
-    return HttpResponse(
-        serializers.serialize("json", crowdfunds), content_type="application/json"
-    )
+    crowdfunds = Crowdfund.objects.all().order_by("-created").values()
+    obj = []
+    for c in crowdfunds:
+        c["profile"] = list(Profile.objects.filter(pk=c["user_id"]).values())[0]
+        c["profile"].pop("user_id")
+        c["profile"]["user"] = list(User.objects.filter(pk=c["user_id"]).values())[0]
+        c["profile"]["user"].pop("password")
+        c["profile"]["user"].pop("last_login")
+        c["profile"]["user"].pop("is_superuser")
+        c["profile"]["user"].pop("is_staff")
+        c["profile"]["user"].pop("is_active")
+        c["profile"]["user"].pop("date_joined")
+        obj.append(c)
+    return JsonResponse(obj, safe=False)
 
 
-@login_required(login_url="/login/")
+@login_required(login_url="/todolist/login/")
 def show_crowdfundings_by_id_json(request, id):
     # reference: https://stackoverflow.com/questions/757022/how-do-you-serialize-a-model-instance-in-django
     crowdfund = Crowdfund.objects.get(pk=id)
@@ -81,12 +88,11 @@ def show_crowdfundings_by_id_json(request, id):
     return HttpResponse(data, content_type="application/json")
 
 
-@login_required(login_url="/login/")
+@login_required(login_url="/todolist/login/")
 def get_user_by_id(request, id):
     profile = Profile.objects.get(user=id)
     user_obj = {
         "username": profile.user.username,
-        "first_name": profile.user.first_name,
         "full_name": profile.user.get_full_name(),
         "email": profile.user.email,
         "telephone": profile.telephone,
