031e07c26760fd5d8679f36540bb10030e32c780
Kristo Jeremy <kristojeremy@gmail.com>
2022-10-28 13:47:57 +0700

    modify html (1)

--- leaderboard/urls.py
+++ leaderboard/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from leaderboard.views import leaderboard
+from leaderboard.views import leaderboard, show_json
 
 app_name = 'leaderboard'
 
 urlpatterns = [
     path('', leaderboard, name='leaderboard'), 
+    path('json/', show_json, name='show_json')
 ]
--- leaderboard/views.py
+++ leaderboard/views.py
@@ -2,10 +2,13 @@ from django.shortcuts import render
 from fitur_autentikasi.models import Profile, User
 from django.shortcuts import render
 from django.db.models import Sum
+from django.core import serializers
+from django.http import HttpResponse
 
 def leaderboard(request):
+    """
     num_of_user = Profile.objects.count()
-    
+
     if num_of_user == 0:
         user_leaderboard = 'The leaderboard is empty'
     elif num_of_user < 10:
@@ -18,3 +21,20 @@ def leaderboard(request):
         ).order_by('-total_points')[:10]
 
     return render(request, 'leaderboard.html', {'user_leaderboard':user_leaderboard})
+    """
+    return render(request, 'leaderboard.html')
+
+def show_json(request):
+    num_of_user = Profile.objects.count()
+
+    if num_of_user < 10:
+        user_leaderboard = Profile.objects.alias(
+        total_points=Sum('poin')
+        ).order_by('-total_points')
+    else:
+        user_leaderboard = Profile.objects.alias(
+        total_points=Sum('poin')
+        ).order_by('-total_points')[:10]
+    
+    data_serializers = serializers.serialize('json', user_leaderboard)
+    return HttpResponse(data_serializers)
