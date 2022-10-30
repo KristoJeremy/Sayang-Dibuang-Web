48b7ded6267ea15c7f84f2a9692be7156023139e
khansajovita <khansajovita@gmail.com>
2022-10-31 01:20:00 +0700

    Tambah fitur review

--- information/urls.py
+++ information/urls.py
@@ -7,7 +7,8 @@ app_name = 'information'
 urlpatterns = [
     path('', index, name='index_information'),
     path('review/', all_review, name='all_review'),
-    path('create/', create_review, name='create_review'),
-    path('delete/<int:id>', delete_review, name='delete_review'),
-    path('<int:id>/', review_detail, name='review_detail')
+    path('review/all/', all_review_json, name="all_review_json"),
+    path('review/create/', create_review, name='create_review'),
+    path('review/<int:id>/delete', delete_review, name='delete_review'),
+    path('review/<int:id>/', review_detail, name='review_detail')
 ]
--- information/views.py
+++ information/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
-from django.http import HttpResponseRedirect
 from django.contrib.auth.decorators import login_required
-from django.urls import reverse
+from django.core import serializers
+from django.http import JsonResponse
 
 from information.forms import *
 from information.models import *
@@ -18,36 +18,49 @@ def index(request):
 
 @login_required(login_url='/login/')
 def create_review(request):
-    form = CreateReview(request.POST)
 
     if request.method == "POST":
+        form = CreateReview(request.POST)
         if form.is_valid():
             new_review = form.save(commit=False)
             new_review.user = Profile.objects.get(user=request.user)
             new_review.save()
             return redirect('information:all_review')
 
-    context = {
-        "form": form
-        }
+    form = CreateReview()
+    context = {"form": form}
     return render(request, "information/createreview.html", context)
 
 def all_review(request):
     return render(request, 'information/lihatreview.html')
 
+def all_review_json(request):
+    list = Review.objects.all().order_by('-created')
+    data = []
+
+    for i in list:
+        # print(i.user)
+        username = i.user.user.username
+        data.append({
+            'username': username, 'judul': i.judul,
+            'deskripsi': i.deskripsi, 'created': i.created, 'pk': i.pk
+            })
+    # print(data)
+    return JsonResponse(data, safe=False)
+
 @login_required(login_url='/login/')
 def review_detail(request,id):
-    # review = Review.objects.select_related('profile').select_related('user').get(id=id)
+    review = Review.objects.select_related('user').get(id=id)
     user = request.user 
 
     context = {
-        # 'review':review,
+        'review':review,
         'user':user
     }
-    return render(request, 'information/review-detail.html')
+    return render(request, 'information/review-detail.html', context)
 
 @login_required(login_url='/login/')
 def delete_review(request, id):
-    review = Review.objects.get(id=id)
+    review = Review.objects.get(user=Profile.objects.get(user=request.user), id=id)
     review.delete()
-    return redirect("information/lihatreview.html")
+    return redirect("information:all_review")
