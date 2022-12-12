3171b7ffb816647fc2142d903e296daf6340752e
Pikatan Arya Bramajati <pikatanab26@gmail.com>
2022-12-12 14:28:01 +0700

    Perbaiki admin.py

--- request_barang_bekas/admin.py
+++ request_barang_bekas/admin.py
@@ -1,7 +1,5 @@
 from django.contrib import admin
 from request_barang_bekas.models import Request, RequestMobile
-from barang_bekas.models import Kategori
 
 admin.site.register(Request)
-admin.site.register(Kategori)
 admin.site.register(RequestMobile)
