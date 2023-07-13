23e716ace588b846f9f8721f0a80f599e098cd80
kristo.jeremy <kristo.jeremy@ui.ac.id>
2023-07-14 00:39:10 +0700

    vercel deployment

--- sayang_dibuang/wsgi.py
+++ sayang_dibuang/wsgi.py
@@ -14,3 +14,5 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sayang_dibuang.settings')
 
 application = get_wsgi_application()
+
+app = application
